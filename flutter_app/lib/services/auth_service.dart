0f9ce47a652259b3ab6ba64143b7dec5b0acf393
your-id <y.dashchuk@studenti.unisa.it>
2026-06-21 19:45:48 +0200

    uwu

--- flutter_app/lib/services/auth_service.dart
+++ flutter_app/lib/services/auth_service.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/user_model.dart';
